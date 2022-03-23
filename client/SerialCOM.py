b7e518f9aa5d1cc41ba0c547ff7b9f811788802e
TangerineVoid <102183226+TangerineVoid@users.noreply.github.com>
2022-03-22 18:19:26 -0600

    Create SerialCOM.py

---
+++ client/SerialCOM.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import socket
+import time, datetime
+import serial
+import os
+
+st = 4
+HOST = "192.168.137.2"  # The server's hostname or IP address
+PORT = 65433  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    print('socket connected')
+    s.connect((HOST, PORT))
+    with serial.Serial(port="COM5", baudrate=115200, bytesize=8, stopbits=serial.STOPBITS_ONE) as sr:
+        print('serial connected')
+        with open('D:/Users/a01280509/Documents/Imager Data/'+'positions_'+str(datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S"))+'.txt', 'a') as f:
+            print('file opened')
+            while 1:
+                s.sendall(b'p\r')
+                data = s.recv(1024)
+                
+                arr = bytes("!Snapshot\r\n", 'utf-8')
+                sr.write(arr)
+
+                f.write(data.decode("ISO-8859-1")+'@'+str(datetime.datetime.now())+'\n')
+                time.sleep(st)
+                print(f"Received", data)
