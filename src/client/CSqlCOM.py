8846d2cdd55f1962c8d400fa281223eca9b0571a
TangerineVoid <13sass@gmail.com>
2022-05-04 16:45:29 -0500

    To be able to insert values to a table the 'connection.commit' was necessary

--- src/client/CSqlCOM.py
+++ src/client/CSqlCOM.py
@@ -24,7 +24,17 @@ def create_database(connection, query):
     except Error as e:
         print(f"The error '{e}' occurred")
 
-connection = create_connection("localhost", "root", "admin", "sm_app")
+def add_value(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Value was added successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+#connection = create_connection("localhost", "root", "admin", "sm_app")
 #connection = create_connection("localhost", "root", "admin")
-create_database_query = "CREATE DATABASE sm_app"
-create_database(connection, create_database_query)
+#create_database_query = "CREATE DATABASE sm_app"
+#create_database(connection, create_database_query)
